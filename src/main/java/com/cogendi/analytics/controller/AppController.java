4728c57a485bc382eb10828f11abdf8b34b2f605
arach <anass.rach@amadeus.com>
2021-01-14 22:29:41 +0100

    Adding creation and editing of a new country.

--- src/main/java/com/cogendi/analytics/controller/AppController.java
+++ src/main/java/com/cogendi/analytics/controller/AppController.java
@@ -5,7 +5,11 @@ import com.cogendi.analytics.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -21,5 +25,29 @@ public class AppController {
     model.addAttribute("countries", countries);
 
     return "index";
+  }
+
+  @RequestMapping("/new")
+  public String showNewProductPage(Model model) {
+    Country country = new Country();
+    model.addAttribute("country", country);
+
+    return "new_country";
+  }
+
+  @RequestMapping(value = "/save", method = RequestMethod.POST)
+  public String saveProduct(@ModelAttribute("country") Country country) {
+    service.save(country);
+
+    return "redirect:/";
+  }
+
+  @RequestMapping("/edit/{id}")
+  public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
+    ModelAndView mav = new ModelAndView("edit_country");
+    Country country = service.get(id);
+    mav.addObject("country", country);
+
+    return mav;
   }
 }
